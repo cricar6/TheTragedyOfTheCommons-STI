8af47fcd3fc7601740ffdf55be19e8ad86de49b5
98101673685 <98101673685@SC309E14.academico.icesi.edu.co>
2018-10-19 04:01:34 -0500

    Conection near to end

---
+++ STI_TragediaDeLosComunes/src/Comunicacion/Player.java
@@ -0,0 +1,45 @@
+package Comunicacion;
+
+public abstract class Player extends Thread {
+
+	protected int poblacion, felicidad, demanda;
+	protected String nombre;
+	protected int season, energiaGeneral, turno;
+	protected String notificacion;
+	protected boolean canPlay = false;
+
+	public Player () {
+		
+	}
+	
+	public void getData ( int poblacion, int demanda, int energiaGeneral ) {
+		this.poblacion = poblacion;
+		this.demanda = demanda;
+		this.energiaGeneral = energiaGeneral;
+		
+		season = 1;
+	}
+	@Override
+	public abstract void run();
+	
+	public abstract void setNombre(String nombre);
+	public abstract void setPoblacion(int poblacion);
+	public abstract void setDemanda(int demanda);
+	public abstract void setSeason(int season);
+
+	
+	public boolean isCanPlay() {
+		return canPlay;
+	}
+
+	public int getTurno() {
+		return turno;
+	}
+
+	public int getSeason () {
+		return season;
+	}
+
+
+	
+}
